f1b035aed6e8ca525965d2fce69bf16e8bf19142
Guilherme Gambeti <guilherme.gambeti@dolby.com>
2023-08-28 12:52:27 +1000

    updated params

--- src/components/VueVideoPlayer.vue
+++ src/components/VueVideoPlayer.vue
@@ -52,7 +52,7 @@ export default defineComponent({
   data() {
     return {
       viewerPluginParams: {
-        accountId: 'xxTzEe',
+        accountId: 'xxxxxx',
         streamName: 'myStreamName',
       }
     }
